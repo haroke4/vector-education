9caa0dbdee424f2fb5f31682c5af8d5289594bd4
amanzholov <freelance1@gmail.com>
2024-05-28 01:34:10 +0500

    hotfix??

--- backend/api_lessons/models/lesson_components/__page_element.py
+++ backend/api_lessons/models/lesson_components/__page_element.py
@@ -1,61 +1,7 @@
 from django.db import models
 from rest_framework.exceptions import ValidationError
 
-from .__component_base import LessonPage, ComponentBase
-
-"""
-Run this file in order to automatically create fields with components.
-"""
-
-
-def func():
-    import importlib
-    import os
-
-    # Get the directory of the models
-    models_dir = os.path.dirname(os.path.realpath(__file__))
-
-    # Get all python files in the directory
-    model_files = [f for f in os.listdir(models_dir) if
-                   os.path.isfile(os.path.join(models_dir, f)) and f.endswith('.py')]
-    code_lines = []
-    added_models = []
-
-    # Import and register each model
-    for model_file in model_files:
-        module_name = model_file[:-3]
-        module = importlib.import_module(f'api_lessons.models.lesson_components.{module_name}')
-        for attr_name in dir(module):
-            attr = getattr(module, attr_name)
-            if isinstance(attr, type) and issubclass(attr, ComponentBase):
-                # if model is abstract, skip it
-                if attr._meta.abstract:
-                    continue
-                if attr in added_models:
-                    continue
-                class_name = str(attr).split('.')[-1][:-2]
-
-                # model_name = class name from camel case to snake case
-                model_name = ''.join(['_' + i.lower() if i.isupper() else i for i in class_name]).lstrip('_')
-
-                app_name = attr._meta.app_label
-                code_lines.append(
-                    f'''{model_name} = models.OneToOneField('{app_name}.{class_name}', on_delete=models.CASCADE, related_name='page_element', blank=True, null=True)''')
-                added_models.append(attr)
-
-    with open(__file__, 'r') as f:
-        this_script = list(f)
-        index = this_script.index('    # components:  :3\n')
-        this_script = this_script[:index + 1]
-
-    with open(__file__, 'w') as f:
-        f.write(f'''{''.join(this_script)[:-1]}
-    {'\n\t'.join(code_lines)}
-    ''')
-
-
-if __name__ == '__main__':
-    func()
+from .__component_base import LessonPage
 
 
 class LessonPageElement(models.Model):
@@ -77,23 +23,14 @@ class LessonPageElement(models.Model):
     order = models.PositiveIntegerField(verbose_name='Порядок элемента на странице')
 
     # components:  :3
-    matching_component = models.OneToOneField('api_lessons.MatchingComponent', on_delete=models.CASCADE,
-                                              related_name='page_element', blank=True, null=True)
-    audio_component = models.OneToOneField('api_lessons.AudioComponent', on_delete=models.CASCADE,
-                                           related_name='page_element', blank=True, null=True)
-    blue_card_component = models.OneToOneField('api_lessons.BlueCardComponent', on_delete=models.CASCADE,
-                                               related_name='page_element', blank=True, null=True)
-    fill_text_component = models.OneToOneField('api_lessons.FillTextComponent', on_delete=models.CASCADE,
-                                               related_name='page_element', blank=True, null=True)
-    image_component = models.OneToOneField('api_lessons.ImageComponent', on_delete=models.CASCADE,
-                                           related_name='page_element', blank=True, null=True)
-    put_in_order_component = models.OneToOneField('api_lessons.PutInOrderComponent', on_delete=models.CASCADE,
-                                                  related_name='page_element', blank=True, null=True)
-    question_component = models.OneToOneField('api_lessons.QuestionComponent', on_delete=models.CASCADE,
-                                              related_name='page_element', blank=True, null=True)
-    record_audio_component = models.OneToOneField('api_lessons.RecordAudioComponent', on_delete=models.CASCADE,
-                                                  related_name='page_element', blank=True, null=True)
-    text_component = models.OneToOneField('api_lessons.TextComponent', on_delete=models.CASCADE,
-                                          related_name='page_element', blank=True, null=True)
-    video_component = models.OneToOneField('api_lessons.VideoComponent', on_delete=models.CASCADE,
-                                           related_name='page_element', blank=True, null=True)
+    matching_component = models.OneToOneField('api_lessons.MatchingComponent', on_delete=models.CASCADE, related_name='page_element', blank=True, null=True)
+    audio_component = models.OneToOneField('api_lessons.AudioComponent', on_delete=models.CASCADE, related_name='page_element', blank=True, null=True)
+    blue_card_component = models.OneToOneField('api_lessons.BlueCardComponent', on_delete=models.CASCADE, related_name='page_element', blank=True, null=True)
+    fill_text_component = models.OneToOneField('api_lessons.FillTextComponent', on_delete=models.CASCADE, related_name='page_element', blank=True, null=True)
+    image_component = models.OneToOneField('api_lessons.ImageComponent', on_delete=models.CASCADE, related_name='page_element', blank=True, null=True)
+    put_in_order_component = models.OneToOneField('api_lessons.PutInOrderComponent', on_delete=models.CASCADE, related_name='page_element', blank=True, null=True)
+    question_component = models.OneToOneField('api_lessons.QuestionComponent', on_delete=models.CASCADE, related_name='page_element', blank=True, null=True)
+    record_audio_component = models.OneToOneField('api_lessons.RecordAudioComponent', on_delete=models.CASCADE, related_name='page_element', blank=True, null=True)
+    text_component = models.OneToOneField('api_lessons.TextComponent', on_delete=models.CASCADE, related_name='page_element', blank=True, null=True)
+    video_component = models.OneToOneField('api_lessons.VideoComponent', on_delete=models.CASCADE, related_name='page_element', blank=True, null=True)
+
